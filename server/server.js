485b4d61295d42d259817f6d79dd4f4e2e0cfe2a
isaack205 <kahuraisaac205@gmail.com>
2025-07-25 14:23:27 +0300

    Change to cors error

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: ["http://localhost:5173", 'https://week-7-devops-deployment-assignment-l2g7.onrender.com'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
